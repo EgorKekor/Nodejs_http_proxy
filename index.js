e5ea56f2896d7ef0013c09f926b3bcf8e9a9cf06
Egor <egor140398@gmail.com>
2020-03-09 23:08:29 +0300

    Https in process.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const https = require('https');
+const net = require('net');
 const url = require('url');
-const port = 8080;
+const basePort = 8080;
+const httpPort = 8081;
+const httpsPort = 8082;
 
 const getProxyResponseHandler = (originalResponse) => {
     return (res) => {
@@ -17,15 +20,13 @@ const getProxyResponseHandler = (originalResponse) => {
     };
 };
 
-
-
 const proxyErrorHandler = (err) => {
     console.error(`problem with request: ${err.message}`);
 }
 
 
 const requestHandler = (request, response) => {
-
+    console.log("1111");
     const urlObject = url.parse(request.url);
 
     const options = {
@@ -34,8 +35,7 @@ const requestHandler = (request, response) => {
         path: urlObject.pathname,
         method: request.method,
         headers: request.headers,
-        };
-
+    };
 
     const proxyRequest = http.request(options);
 
@@ -52,14 +52,48 @@ const requestHandler = (request, response) => {
     proxyRequest.on('error', proxyErrorHandler);
 };
 
+httpsRequestHandler = (request, response) => {
+    console.log("2222");
+    response.end("papi para pa pa pu");
+}
 
-const server = new http.Server();
-server.on('request', requestHandler);
 
-
-server.listen(port, (err) => {
+const httpServer = new http.Server();
+httpServer.on('request', requestHandler);
+httpServer.listen(httpPort, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
-    } console.log(`server is listening on ${port}`)
+        return console.log('something bad happened in http', err)
+    }
 });
+
+const httpsServer = new https.Server();
+httpsServer.on('request', requestHandler);
+httpsServer.listen(httpsPort, (err) => {
+    if (err) {
+        return console.log('something bad happened in https', err)
+    }
+});
+
+
+
+net.createServer((conn) => {
+    conn.once('data', function (buf) {
+        const proxyPort = (buf[0] === 67) ? httpsPort : httpPort;
+        console.log(buf);
+        console.log(buf[0]);
+        var proxy = net.createConnection(proxyPort, () => {
+            proxy.write(buf);
+            conn.pipe(proxy).pipe(conn);
+        });
+    });
+}).listen(basePort, (err) => {
+    if (err) {
+        return console.log('something bad happened in main', err)
+    } console.log(`httpServer is listening on ${basePort}`)
+});
+
+
+//https.createServer(httpsOptions, httpsConnection).listen(httpsAddress);
+
+
 
