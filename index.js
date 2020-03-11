8b336ad6fc04c49ad2bb217658e55cb66616468b
EgorKekor <egor140398@gmail.com>
2020-03-11 21:26:44 +0300

    Final.

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 const http = require('http');
 const tls = require('tls');
-const net = require('net');
 const url = require('url');
 var fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const httpPort = 8080;
-const tlsPort = 8081;
 
+const execOneCommand = async (command, i) => {
+    try {
+        const { stdout, stderr } = await exec(command, { cwd: './conf' });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const execAllCommands = async (commands) => {
+    await execOneCommand(commands[0], 0);
+    await execOneCommand(commands[1], 1);
+    await execOneCommand(commands[2], 2);
+    await execOneCommand(commands[3], 3);
+    await execOneCommand(commands[4], 4);
+    return;
+}
+
+const generateOptionsByHostName = async (hostname) => {
+    if (!(fs.existsSync("./conf/" + hostname + ".key") && fs.existsSync("./conf/" + hostname + ".crt"))) {
+        const commands = [
+            "openssl genrsa -out " + hostname + ".key " + " 2048",
+            "openssl req -new -sha256 -key " + hostname + ".key -subj \"/C=US/ST=CA/O=MyOrg, Inc./CN=" + hostname + "\"" + " -out " + hostname + ".csr",
+            "openssl req -in " + hostname + ".csr -noout -text",
+            "openssl x509 -req -in " + hostname + ".csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out " + hostname + ".crt -days 500 -sha256",
+            "openssl x509 -in " + hostname + ".crt -text -noout"];
+        await execAllCommands(commands);
+    }
+    const options = {
+        key: (() => {
+            try {
+                return fs.readFileSync("./conf/" + hostname + ".key")
+            } catch (e) {
+                console.error(e);
+            }
+        })(),
+        cert: (() => {
+            try {
+                return fs.readFileSync("./conf/" + hostname + ".crt")
+            } catch (e) {
+                console.error(e);
+            }
+        })(),
+        rejectUnauthorized: false
+    };
+    return options;
+}
 
 const requestHandler = (request, clientResponse) => {
-    console.log("HTTP handler");
+    //console.log("HTTP handler");
     const urlObject = url.parse(request.url);
 
     const options = {
@@ -23,89 +69,65 @@ const requestHandler = (request, clientResponse) => {
         clientResponse.writeHead(serverResponse.statusCode, serverResponse.headers);
         serverResponse.pipe(clientResponse);
     });
+
+    proxyRequest.on("error", (err) => {
+        //console.log(`Http proxyRequest error: ${err}`);
+    });
+
+    proxyRequest.on("error", (err) => {
+        //console.log(`Http request error: ${err}`);
+    });
+
     request.pipe(proxyRequest);
 };
 
-
-
-const tlsServerOptions = {
-    key: fs.readFileSync('rootCA.key'),
-    cert: fs.readFileSync('rootCA.crt'),
-    passphrase: 'qqqq'
-};
-
-const tlsUncoderServer = tls.createServer(tlsServerOptions, (cltSocket) => {
-    cltSocket.on('data', (chunk) => {
-        console.log(chunk.toString('ascii'));
-    });
-});
-
-tlsUncoderServer.listen(tlsPort, (err) => {
-    if (err) {
-        return console.log('something bad happened in tls server', err)
-    }
-    console.log(`tls decoder server listen on port ${httpPort}`);
-});
-
-
-
 const proxy = http.createServer(requestHandler);
-proxy.on('connect', (req, cltSocket, head) => {
-    const { port, hostname } = new url.URL(`http://${req.url}`);
-    // console.log("CONNECT method");
-    // console.log(`port:${port} hostname:${hostname}`);
+proxy.on('error', (err) => {
+    //console.log(`Proxy server error: ${err}`);
+});
 
-    const tlsOptions = {
-        key: fs.readFileSync('rootCA.key'),
-        cert: fs.readFileSync('rootCA.crt'),
-        passphrase: 'qqqq',
-        rejectUnauthorized: false
-    };
+proxy.on('connect', async (req, cltSocket, head) => {
+    cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+        'Proxy-agent: Node.js-Proxy\r\n' +
+        '\r\n', async () => {
+            const tlsOptions = {
+                key: fs.readFileSync('./conf/rootCA.key'),
+                cert: fs.readFileSync('./conf/rootCA.crt'),
+                isServer: true,
+            };
+            
+            const tlsSocket = new tls.TLSSocket(cltSocket, tlsOptions);
+            tlsSocket.on('error', (err) => {
+                console.log(`$TLSSocket error: ${err}`);
+            });
 
-    const decoderSocket = tls.connect(8081, '127.0.0.1', tlsOptions, () => {
+            const { port, hostname } = new url.URL(`http://${req.url}`);
+            let options;
+            try {
+                options = await generateOptionsByHostName(hostname);
+            } catch (err) {
+                console.log(err);
+            }
 
-        cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-            'Proxy-agent: Node.js-Proxy\r\n' +
-            '\r\n');
-
-
-        decoderSocket.write(head);
-        cltSocket.pipe(decoderSocket);
-
-        //decoderSocket.pipe(cltSocket);
-    });
-
-
-
-    // const options = {
-    //     key: fs.readFileSync('./anita_crt/google_com.key'),
-    //     cert: fs.readFileSync('./anita_crt/google_com.crt'),
-    //     rejectUnauthorized: false
-    // };
-
-    // const srvSocket = tls.connect(port || 80, hostname, options, () => {
-    //     if (srvSocket.authorized) {
-    //         console.log("Connection authorized by a Certificate Authority.");
-    //     } else {
-    //         return;
-    //     }
-
-    //     // cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-    //     //     'Proxy-agent: Node.js-Proxy\r\n' +
-    //     //     '\r\n');
-
-    //     srvSocket.write(head);
-    //     srvSocket.pipe(cltSocket);
-    //     cltSocket.pipe(srvSocket);
-
-    //     // cltSocket.on('data', (chunk) => {
-    //     //     console.log(chunk.toString('ascii'));
-    //     // });
-
-    //     // srvSocket.on('data', (chunk) => {
-    //     //     console.log(chunk.toString('ascii'));
-    //     // });
-    // });
+            try {
+                const srvSocket = tls.connect(port || 80, hostname, options, () => {
+                    console.log(`hostname:${hostname}`);
+                    if (srvSocket.authorized) {
+                        console.log("Connection authorized by a Certificate Authority.");
+                    } else {
+                        console.log("Connection authorized error!");
+                    }
+                    srvSocket.write(head);
+                    srvSocket.pipe(tlsSocket);
+                    tlsSocket.pipe(srvSocket);
+                });
+                srvSocket.on('error', (err) => {
+                    console.log(`$SrvSocket error: ${err}`);
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        });
 });
 
 proxy.listen(httpPort, (err) => {
